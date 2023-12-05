ce824c1facd8b79967b022cfe80c154d4c023f31
NeoMatlala <neomatlala70@gmail.com>
2023-12-05 21:39:46 +0200

    Vue client project setup with tailwind

---
+++ EmployeeManage.UI/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-gray-100 ">
+    <NuxtLayout >
+      <Header />
+      <NuxtPage class="py-5 px-5 md:px-0 mt-5 min-h-[70vh]"/>
+      <Footer />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ EmployeeManage.UI/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-center font-medium text-6xl">Welcome to the client</h1>
+</template>
