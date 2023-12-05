43238ec140eabf46dbef11a18fbe941e029b9f24
NeoMatlala <neomatlala70@gmail.com>
2023-12-05 22:11:52 +0200

    Add cors || READ all departments

--- EmployeeManage.UI/pages/index.vue
+++ EmployeeManage.UI/pages/index.vue
@@ -1,3 +1,36 @@
 <template>
-    <h1 class="text-center font-medium text-6xl">Welcome to the client</h1>
+    <div class="max-w-7xl mx-auto">
+        <h1 class="text-center font-medium text-6xl mb-16">Dashboard</h1>
+        <ul v-for="department in departments">
+            <li>{{ department.departmentName }} - {{ department.manager }}</li>
+        </ul>
+    </div>
+    
 </template>
+
+<script>
+import axios from 'axios';
+
+export default{
+    data() {
+        return {
+        departments: []
+        };
+    },
+    created(){
+    this.fetchDepartments();
+    },
+    methods: {
+        async fetchDepartments() {
+            try{
+                const response = await axios.get('https://localhost:7244/api/Department/GetDepartments');
+                console.log(response.data);
+                this.departments = response.data
+            } catch(error) {
+                console.error('Error fetching departments:', error.message);
+            }
+        }
+    }
+}
+
+</script>
