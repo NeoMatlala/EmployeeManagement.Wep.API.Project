4dad7b42789463c2e689a676109685debcedb849
NeoMatlala <neomatlala70@gmail.com>
2023-12-08 14:31:27 +0200

    FE: Read all employees

--- EmployeeManage.UI/pages/index.vue
+++ EmployeeManage.UI/pages/index.vue
@@ -3,12 +3,12 @@
         <h1 class="font-medium text-3xl text-slate-800 mb-16">Dashboard</h1>
 
         <div class="mt-10 flex justify-start items-center gap-10">
-            <a href="#" class="block max-w-[200px] p-16 bg-white border border-gray-200 rounded-lg hover:bg-gray-100">
-                <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">56</h5>
+            <NuxtLink to="/employees" class="block max-w-[200px] p-16 bg-white border border-gray-200 rounded-lg hover:bg-gray-100">
+                <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{{employeeCount}}</h5>
                 <p class="font-normal text-gray-700 dark:text-gray-400">Employees</p>
-            </a>
+            </NuxtLink>
             <NuxtLink to="/departments" class="block max-w-[200px] p-16 bg-white border border-gray-200 rounded-lg hover:bg-gray-100">
-                <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{{count}}</h5>
+                <h5 class="mb-2 text-5xl font-bold tracking-tight text-gray-900 dark:text-white">{{departmentCount}}</h5>
                 <p class="font-normal text-gray-700 dark:text-gray-400">Departments</p>
             </NuxtLink>
         </div>
@@ -21,19 +21,29 @@ import axios from 'axios';
 export default{
     data() {
         return {
-        count: 0,
+        departmentCount: 0,
+        employeeCount: 0,
         };
     },
     created(){
     this.fetchDepartmentsCount();
+    this.fetchEmployeeCount();
     },
     methods: {
         async fetchDepartmentsCount() {
             try{
                 const response = await axios.get('https://localhost:7244/api/Department/GetDepartments');
-                this.count = response.data.length
+                this.departmentCount = response.data.length
             } catch(error) {
                 console.error('Error fetching departments:', error.message);
+            }
+        },
+        async fetchEmployeeCount(){
+            try{
+                var response = await axios.get("https://localhost:7244/api/Employee")
+                this.employeeCount = response.data.length
+            } catch(error){
+                console.log("Error getting employee count:", error.message)
             }
         }
     }
