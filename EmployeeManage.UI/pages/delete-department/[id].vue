a3201348162381b9306c9f2054b8d832cd71c50c
NeoMatlala <neomatlala70@gmail.com>
2023-12-10 14:37:16 +0200

    delete user and department modal

--- EmployeeManage.UI/pages/delete-department/[id].vue
+++ EmployeeManage.UI/pages/delete-department/[id].vue
@@ -18,8 +18,26 @@
                 </div>  
             </div>
             <div class="flex justify-end items-center">
-                <button type="submit" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Delete department</button>
+                <a id="button" class="text-white hover:cursor-pointer bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Delete department</a>
                 <NuxtLink to="/departments" data-modal-hide="default-modal" type="button" class="ms-3 text-gray-500 bg-gray-50 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Cancel</NuxtLink>
+            </div>
+
+            <!-- delete modal -->
+            <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative p-4 w-full max-w-md max-h-full">
+                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                        <div class="p-4 md:p-5 text-center">
+                            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                            </svg>
+                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete the <br>{{department.departmentName}} department ?</h3>
+                            <button type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
+                                Yes, I'm sure
+                            </button>
+                            <button id="closeButton" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">No, cancel</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </form>
     </div>
@@ -29,6 +47,8 @@
 import axios from 'axios'
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { onMounted } from 'vue'
+import { Modal } from 'flowbite'
 
 export default{
     data(){
@@ -38,6 +58,21 @@ export default{
     },
     setup(){
         const id = ref(useRoute().params.id);
+
+        onMounted(() => {
+            const $buttonElement = document.querySelector('#button');
+            const $modalElement = document.querySelector('#popup-modal');
+            const $closeButton = document.querySelector('#closeButton');
+            const modalOptions = {
+                backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40'
+            }
+            if ($modalElement) {
+                const modal = new Modal($modalElement, modalOptions);
+                $buttonElement.addEventListener('click', () => modal.toggle());
+                $closeButton.addEventListener('click', () => modal.hide());
+            }
+        })
+
         return {
             id,
         };
@@ -50,16 +85,27 @@ export default{
             try{
                 var response = await axios.get(`https://localhost:7244/api/Department/GetDepartment/${this.id}`);
                 this.department = response.data
-                console.log(response.data)
+                //console.log(response.data)
             } catch(error){
                 console.log('Error fetching departments', error.message)
             }
         },
         async handleDelete(){
-            console.log(this.id)
+            const $modalElement = document.querySelector('#popup-modal');
+            const $closeButton = document.querySelector('#closeButton');
+            const modalOptions = {
+                backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40'
+            }
+
             try{
                 var response = await axios.delete(`https://localhost:7244/api/Department/DeleteDepartment/${this.id}`)
-                console.log(response.data)
+                //console.log(response.data)
+
+                if ($modalElement) {
+                    const modal = new Modal($modalElement, modalOptions);
+                    $closeButton.addEventListener('click', () => modal.hide());
+                }
+
                 this.$router.replace('/departments');
             } catch(error) {
                 console.log("Error delete department:", error.message)
